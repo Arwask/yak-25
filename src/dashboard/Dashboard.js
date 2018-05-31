206ca68dc588f034a400ef1ff8f0d4b9657dd358
Arwa <arwa.it15@gmail.com>
2018-05-31 10:34:59 -0500

    add dashboard component-which will be Home

---
+++ src/dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap';
+import './Dashboard.css';
+
+class Dashboard extends Component {
+  render() {
+    return <div>This is Dashboard Component</div>;
+  }
+}
+
+export default Dashboard;
