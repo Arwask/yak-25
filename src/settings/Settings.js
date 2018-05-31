7a332c8e0f5aba4b583d8cea92571ee2e3a41f26
Arwa <arwa.it15@gmail.com>
2018-05-31 13:10:09 -0500

    add bare minimum code to get links up and running

---
+++ src/settings/Settings.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap';
+import './Settings.css';
+
+class Settings extends Component {
+  render() {
+    return <div>This is Settings Component</div>;
+  }
+}
+
+export default Settings;
